4d8dca2513798e025dceff6caae4ece0ad6ea9c8
Gavin Gao <cggos@outlook.com>
2024-11-23 16:59:49 +0800

    add zzz-default-settings for ref

--- customize.sh
+++ customize.sh
@@ -3,11 +3,12 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.55.55/g' package/base-files/files/bin/config_generate
 
+# Modify hostname
+sed -i 's/OpenWrt/CG-OpenWRT/g' package/base-files/files/bin/config_generate
+
 # Clear the login password
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-sed -i 's/OpenWrt/CG-OpenWRT/g' package/base-files/files/bin/config_generate
